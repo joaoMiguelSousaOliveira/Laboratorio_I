f8a6260f100cc526beb6c641099ad0d241924710
João Miguel <166736092+joaoMiguelSousaOliveira@users.noreply.github.com>
2025-09-15 22:56:56 -0300

    Class item

---
+++ Item.java
@@ -0,0 +1,20 @@
+
+public class Item {
+
+    String nomeItem;
+    double precoItem;
+
+    public Item(String nomeItem, double precoItem) {
+        this.nomeItem = nomeItem;
+        this.precoItem = precoItem;
+    }
+
+    public String getNomeItem() {
+        return nomeItem;
+    }
+
+    public double getPrecoItem() {
+        return precoItem;
+    }
+
+}
